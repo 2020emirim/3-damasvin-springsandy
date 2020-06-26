a310c3758bcdc5c5de26767a0b3a1a733a3fa70b
s2018w26 <s2018w26@e-mirim.hs.kr>
2020-06-26 15:41:04 +0900

    menu/views DrinkListView, CoffeeCreateView, BubbleteaCreateView, DrinkCreateView, DrinkDeleteView

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from menu.models import Drink, Coffee, Bubbletea
+
+
+class DrinkListVIew(ListView):
+    model = Drink
+    paginate_by = 3
+
+
+class CoffeeCreateView(CreateView):
+    model = Coffee
+    fields = '__all__'
+    template_name = 'drink_create.html'
+    success_url = reverse_lazy('menu:list')
+
+class BubbleteaCreateView(CreateView):
+    model = Bubbletea
+    fields = '__all__'
+    template_name = 'drink_create.html'
+    success_url = reverse_lazy('menu:list')
+
+class DrinkUpdateView(UpdateView):
+    model = Drink
+    fields = '__all__'
+    template_name_suffix = 'drink_update.html'
+    success_url = reverse_lazy('menu:list')
+
+class DrinkDeleteView(DeleteView):
+    model = Drink
+    success_url = reverse_lazy('menu:list')
