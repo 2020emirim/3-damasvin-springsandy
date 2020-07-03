1b79c7d1c227359997576eabb70ee4118823eae1
s2018w26 <s2018w26@e-mirim.hs.kr>
2020-07-03 14:55:53 +0900

    menu/views initial

--- menu/views.py
+++ menu/views.py
@@ -5,27 +5,29 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from menu.models import Drink, Coffee, Bubbletea
 
 
-class DrinkListVIew(ListView):
+class DrinkListView(ListView):
     model = Drink
-    paginate_by = 3
+    # paginate_by = 3
 
 
 class CoffeeCreateView(CreateView):
     model = Coffee
     fields = '__all__'
-    template_name = 'drink_create.html'
+    template_name = 'menu/drink_create.html'
     success_url = reverse_lazy('menu:list')
+    initial = {'category':'Coffee'}
 
 class BubbleteaCreateView(CreateView):
     model = Bubbletea
     fields = '__all__'
-    template_name = 'drink_create.html'
+    template_name = 'menu/drink_create.html'
     success_url = reverse_lazy('menu:list')
+    initial = {'category': 'Bubbletea'}
 
 class DrinkUpdateView(UpdateView):
     model = Drink
     fields = '__all__'
-    template_name_suffix = 'drink_update.html'
+    template_name = 'menu/drink_update.html'
     success_url = reverse_lazy('menu:list')
 
 class DrinkDeleteView(DeleteView):
